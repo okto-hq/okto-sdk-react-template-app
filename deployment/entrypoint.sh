99890164470b51d5d48c0d2e312beaf55caadf58
Nishith <nishith.kulshrestha@coindcx.com>
2024-10-09 17:14:26 +0530

    test

--- deployment/entrypoint.sh
+++ deployment/entrypoint.sh
@@ -2,6 +2,4 @@
 set -e
 
 # start application
-echo "REACT_APP_OKTO_CLIENT_API_KEY=$REACT_APP_OKTO_CLIENT_API_KEY" > .env
-echo "REACT_APP_GOOGLE_CLIENT_ID=$REACT_APP_GOOGLE_CLIENT_ID" >> .env
 npm run start
